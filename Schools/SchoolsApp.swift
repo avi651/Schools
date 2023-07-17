d7e2f8bf1ffdc076e17b05099e5722e9aef40fdf
= <Avinash.Kumar@baptisthealth.net>
2023-07-17 15:02:18 +0530

    Initial Commit

---
+++ Schools/SchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolsApp.swift
+//  Schools
+//
+//  Created by Avinash Kumar on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
